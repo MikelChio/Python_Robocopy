c83f16aee66bdb2e5fa35806962a9564f888a7f8
mikelchio <mikel.l.chio@gmail.com>
2024-12-14 11:41:38 -0800

    pip no idea
    Co-authored-by: Adam Osborne <alovapd@users.noreply.github.com>

---
+++ chio no idea.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import ttk
+import ttkbootstrap as ttk
+
+def convert():
+    mile_input = entry_int.get()
+    km_output = mile_input *1.61
+    output_string.set(km_output)
+
+#Window
+window = tk.Tk()
+window.title('Demo')
+window.geometry('300x150')
+
+#title
+title_label = ttk.Label(master = window, text = 'Miles to kilometers', font = 'Calibri 24 bold')
+title_label.pack()
+
+#input field
+input_frame = tk.Frame(master = window)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master= input_frame, textvariable = entry_int)
+button = ttk.Button(master = input_frame, text = 'Convert', command = convert)
+entry.pack(side = 'left')
+button.pack(side = 'left')
+input_frame.pack(pady = 10)
+
+# output
+output_string = tk.StringVar()
+output_label = ttk.Label(
+    master = window, 
+    text = 'Output', 
+    font = 'Calibri 24', 
+    textvariable=output_string)
+output_label.pack()
+# run
+window.mainloop()
